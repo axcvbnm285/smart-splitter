1365b84aae32a42d93400b94b4ba836e2c0a479b
Abhinav Raj Verma <abhinavrajverma@Abhinavs-MacBook-Air.local>
2026-02-26 02:01:06 +0530

    Use env variable for API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://smart-splitter.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
